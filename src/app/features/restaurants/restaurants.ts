e38276ceedbeda092256e3a5aa6fb8f53d50d970
Mohsin Zaidi <mozak099@gmail.com>
2025-09-02 10:06:39 +0500

    working

--- src/app/features/restaurants/restaurants.ts
+++ src/app/features/restaurants/restaurants.ts
@@ -1,28 +1,43 @@
 import { Component, computed, OnInit, signal } from '@angular/core';
 import { Restaurant } from '../../models/restaurant';
 import { ApiService } from '../../services/api-service';
-import { Observable } from 'rxjs';
+import { filter, map, Observable, of, startWith, subscribeOn } from 'rxjs';
 import { MatList, MatListModule } from "@angular/material/list";
 import { AsyncPipe } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRippleModule } from '@angular/material/core';
 import { RouterLink } from '@angular/router';
 import { LoadingSpinner } from "../../components/loading-spinner/loading-spinner";
+import { MatInput, MatInputModule } from "@angular/material/input";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurants',
-  imports: [AsyncPipe, MatList, MatListModule, MatProgressSpinnerModule, MatRippleModule, RouterLink, LoadingSpinner],
+  imports: [AsyncPipe, MatList, MatListModule, MatProgressSpinnerModule, MatRippleModule, RouterLink, LoadingSpinner, MatInput, MatInputModule, ReactiveFormsModule],
   templateUrl: './restaurants.html',
   styleUrl: './restaurants.scss'
 })
 export class Restaurants {
   value$ = signal(0);
   restaurants$?: Observable<Restaurant[]>;
+  formControl = new FormControl('');
+  filteredItems$?: Observable<Restaurant[]>;
 
-  constructor(private service: ApiService) { }
+  constructor(private service: ApiService) {
+    this.formControl.valueChanges.subscribe((search) => {
+      this.filteredItems$ = this.restaurants$?.pipe(map((r) => {
+        return r.filter((value2, index) => {
+          if (search)
+            return value2.Name.toLowerCase().includes(search.toLowerCase())
+          return of([]);
+        })
+      }))
+    });
+  }
 
   ngOnInit(): void {
     this.restaurants$ = this.service.getRestaurants();
+    this.filteredItems$ = this.restaurants$;
   }
 
   increment(): void {
